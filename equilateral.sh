ae5a6e96d4e6c5faef4b057857821f22fe7ff7f4
jaguhdeveloper@gmail.com <geraldwainaina25@gmail.com>
2023-09-03 08:52:35 +0300

    Create equilateral.sh

---
+++ equilateral.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+read x
+read y
+read z
+
+if [ "$x" -eq "$y" ] && [ "$y" -eq "$z" ]; then
+        echo "EQUILATERAL"
+    elif [ "$x" -eq "$y" ] || [ "$x" -eq "$z" ] || [ "$y" -eq "$z" ]; then
+        echo "ISOSCELES"
+    else
+        echo "SCALENE"
+    fi
